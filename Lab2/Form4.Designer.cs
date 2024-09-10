4f0860c4d1cf6d8f0caadffa3d08d8babc34fa53
ALSolis03 <analia.solis@utp.ac.pa>
2024-09-10 08:59:30 -0500

    creadno botones de opción

---
+++ Lab2/Form4.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Lab2
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(96, 69);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(149, 60);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "button1";
+            this.bt1.UseVisualStyleBackColor = true;
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(301, 69);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(149, 60);
+            this.bt2.TabIndex = 1;
+            this.bt2.Text = "button2";
+            this.bt2.UseVisualStyleBackColor = true;
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(508, 69);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(149, 60);
+            this.bt3.TabIndex = 2;
+            this.bt3.Text = "button3";
+            this.bt3.UseVisualStyleBackColor = true;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+    }
+}
